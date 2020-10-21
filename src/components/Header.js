5249fb395b54187db90b8b6733a7236e2c1c3351
jamesmp88 <jamesmprice@hotmail.co.uk>
2020-10-21 18:11:29 +0100

    Adding all images needed and header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import covid19 from '../Media/logo.png';
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img id="logo" alt="Cartoon covid-19" src={covid19}></img>
+      </div>
+      <h1 className="title">Covid-19 Tracker</h1>
+      <p>Find the latest figures from your area</p>
+    </header>
+  );
+}
+
+export default Header
