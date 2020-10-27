b3be27ce2653f2c0ddb653eab8e719212e879449
jamesmp88 <jamesmprice@hotmail.co.uk>
2020-10-27 23:56:03 +0000

    Updates

---
+++ precache-manifest.0bb1ec91597c5959c1fc0caa747d9d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b14f85a7a5670c92bb6f233c5452ba6",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "0ab5697a8a3009022ea0",
+    "url": "/covid/static/css/main.45c009e9.chunk.css"
+  },
+  {
+    "revision": "2d9d382ac326d6f13aa7",
+    "url": "/covid/static/js/2.32859f0c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/covid/static/js/2.32859f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab5697a8a3009022ea0",
+    "url": "/covid/static/js/main.2461a507.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  },
+  {
+    "revision": "f973ee1967335e43439c8e99356394ce",
+    "url": "/covid/static/media/logo.f973ee19.png"
+  }
+]);
